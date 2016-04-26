d889bad15fffb00bd58deea4afe806d30ac0078f
Sunil <sunhick@gmail.com>
2016-04-25 19:28:39 -0600

    tried multinomial logistic regression model for classification

--- src/genre_classification.py
+++ src/genre_classification.py
@@ -30,6 +30,8 @@ from pyspark.mllib.util import MLUtils
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.mllib.classification import SVMModel
 from pyspark.ml.classification import LogisticRegression
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRegressionModel
+from pyspark.mllib.regression import LabeledPoint
 
 
 """
@@ -45,7 +47,7 @@ genre,track_id,artist_name,title,loudness,tempo,time_signature,key,mode,duration
   0    1         2			3		4		5		6			7	8		9		10			11			12			13			14			15			16			17			18			19				20			21			22			23			24			25			26			27			28			29			30			31			32			33
 """
 
-def parse(line):
+def parseForRandomForest(line):
 	line = line.split(',')
 	genre = line[0]
 	tid = line[1]
@@ -55,18 +57,41 @@ def parse(line):
 	key = float(line[7])
 	mode = float(line[8])
 	duration = float(line[9])
-
 	avgtimbre = [float(line[i]) for i in range(10, 22)]
 	vartimbre = [float(line[i]) for i in range(22, 34)]
-	
 	features = [loudness, tempo, timesig, key, mode, duration]
-	features.extend(avgtimbre)
-	features.extend(vartimbre)
+	#features.extend(avgtimbre)
+	#features.extend(vartimbre)
 	# t1 = float
 	#return {'genre': genre, 'tid': tid, 'loudness': loudness, 'tempo': tempo, 'time_signature': timesig,
 	#		'key': key, 'mode': mode, 'duration': duration}
 	#return {'hash': int(hash(tid) & 0xfffffff), 'genre': genre, 'tid': tid, 'features': np.array([loudness, tempo, timesig, key, mode, duration])}
 	return [genre, tid, Vectors.dense(features)]
+
+global genCats
+genCats = dict({u'jazz and blues':1, u'classic pop and rock':2, u'classical':3, u'punk':4, u'metal':5, u'pop':6, u'dance and electronica':7, u'hip-hop':8, u'soul and reggae':9, u'folk':0})
+def parseForLogit(line):
+	line = line.split(',')
+	genre = line[0]
+	# genre to category label (should be integer for logit)
+	gid = genCats[genre]
+	tid = line[1]
+	loudness = float(line[4])
+	tempo = float(line[5])
+	timesig = float(line[6])
+	key = float(line[7])
+	mode = float(line[8])
+	duration = float(line[9])
+	avgtimbre = [float(line[i]) for i in range(10, 22)]
+	vartimbre = [float(line[i]) for i in range(22, 34)]
+	features = [loudness, tempo, timesig, key, mode, duration]
+	#features.extend(avgtimbre)
+	#features.extend(vartimbre)
+	# t1 = float
+	#return {'genre': genre, 'tid': tid, 'loudness': loudness, 'tempo': tempo, 'time_signature': timesig,
+	#		'key': key, 'mode': mode, 'duration': duration}
+	#return {'hash': int(hash(tid) & 0xfffffff), 'genre': genre, 'tid': tid, 'features': np.array([loudness, tempo, timesig, key, mode, duration])}
+	return LabeledPoint(gid, Vectors.dense(features))
 
 def silentMode(ctx):
 		print 'Turning on silent mode'
@@ -75,16 +100,9 @@ def silentMode(ctx):
 		logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
 		logger.LogManager.getLogger("INFO").setLevel(logger.Level.OFF)
 
-def main(args):
-	conf = ps.SparkConf().setAppName('genre_classification').setMaster('local')
-	ctx = ps.SparkContext(conf=conf)
-
-	silentMode(ctx)
+def RunRandomForest(tf, ctx):
 	sqlContext = SQLContext(ctx)
-
-	tf = ctx.textFile('/Users/Sunny/prv/github/music-cognita/data/MillionSongSubset/AdditionalFiles/msd_genre_dataset.txt')
-	rdd = tf.map(parse)
-
+	rdd = tf.map(parseForRandomForest)
 	# The schema is encoded in a string.
 	schema = ['genre', 'track_id', 'features']
 	# Apply the schema to the RDD.
@@ -98,7 +116,7 @@ def main(args):
 
 	labelConverter = IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
 
-	rfc = RandomForestClassifier().setMaxDepth(10).setNumTrees(3).setLabelCol("indexedLabel").setFeaturesCol("features")
+	rfc = RandomForestClassifier().setMaxDepth(10).setNumTrees(2).setLabelCol("indexedLabel").setFeaturesCol("features")
 	#rfc = SVMModel([.5, 10, 20], 5)
 	#rfc = LogisticRegression(maxIter=10, regParam=0.01).setLabelCol("indexedLabel").setFeaturesCol("features")
 
@@ -110,11 +128,38 @@ def main(args):
 
 	evaluator = MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("precision")
 	accuracy = evaluator.evaluate(predictions)
-	print "Test Error = ", (1.0 - accuracy)
+	print 'Accuracy of RandomForest = ', accuracy * 100
+	print "Test Error = ", (1.0 - accuracy) * 100
+
+def RunLogit(tf):
+	rdd = tf.map(parseForLogit)
+	train, test = rdd.randomSplit([.8, .2])
+	numCat = len(genCats)
+	#weights = [0.] * numCat
+	model = LogisticRegressionWithLBFGS.train(train, numClasses=numCat)
+	predictionAndLabel = test.map(lambda p: (model.predict(p.features), p.label))
+
+	accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
+
+	print 'Accuracy of Logit = ', accuracy * 100
+	print "Test Error = ", (1.0 - accuracy) * 100
+
+def main(args):
+	conf = ps.SparkConf().setAppName('genre_classification').setMaster('local')
+	ctx = ps.SparkContext(conf=conf)
+
+	silentMode(ctx)
+	tf = ctx.textFile('/Users/Sunny/prv/github/music-cognita/data/MillionSongSubset/AdditionalFiles/msd_genre_dataset.txt')
+
+	if (args.model == 'Logit'):
+		RunLogit(tf)
+	else:
+		RunRandomForest(tf, ctx)
 
 if __name__ == '__main__':
 	parser  = argparse.ArgumentParser(description='Genre classification')
-	parser.add_argument('--file', type=str, help='python genre_classification <path_to_dataset>')
+	parser.add_argument('-f', '--file', type=str, help='python genre_classification <path_to_dataset>')
+	parser.add_argument('-m', '--model', type=str, choices=['RandomForest', 'Logit'], default='RandomForest')
 	parser.add_argument('-v', '--verbose', action='count', help='verbosity')
 
 	args = parser.parse_args()
--- src/parser.py
+++ src/parser.py
@@ -10,3 +10,48 @@ def parse_line(line):
     song = parse_id(line[1])
     rating = float(line[2])
     return {'user':user, 'song':song, 'rating':rating}
+
+def parseForRandomForest(line):
+	line = line.split(',')
+	genre = line[0]
+	tid = line[1]
+	loudness = float(line[4])
+	tempo = float(line[5])
+	timesig = float(line[6])
+	key = float(line[7])
+	mode = float(line[8])
+	duration = float(line[9])
+	avgtimbre = [float(line[i]) for i in range(10, 22)]
+	vartimbre = [float(line[i]) for i in range(22, 34)]
+	features = [loudness, tempo, timesig, key, mode, duration]
+	features.extend(avgtimbre)
+	features.extend(vartimbre)
+	# t1 = float
+	#return {'genre': genre, 'tid': tid, 'loudness': loudness, 'tempo': tempo, 'time_signature': timesig,
+	#		'key': key, 'mode': mode, 'duration': duration}
+	#return {'hash': int(hash(tid) & 0xfffffff), 'genre': genre, 'tid': tid, 'features': np.array([loudness, tempo, timesig, key, mode, duration])}
+	return [genre, tid, Vectors.dense(features)]
+
+global ff
+ff = dict({u'jazz and blues':1, u'classic pop and rock':2, u'classical':3, u'punk':4, u'metal':5, u'pop':6, u'dance and electronica':7, u'hip-hop':8, u'soul and reggae':9, u'folk':0})
+def parse(line):
+	line = line.split(',')
+	genre = line[0]
+	gid = ff[genre]
+	tid = line[1]
+	loudness = float(line[4])
+	tempo = float(line[5])
+	timesig = float(line[6])
+	key = float(line[7])
+	mode = float(line[8])
+	duration = float(line[9])
+	avgtimbre = [float(line[i]) for i in range(10, 22)]
+	vartimbre = [float(line[i]) for i in range(22, 34)]
+	features = [loudness, tempo, timesig, key, mode, duration]
+	features.extend(avgtimbre)
+	features.extend(vartimbre)
+	# t1 = float
+	#return {'genre': genre, 'tid': tid, 'loudness': loudness, 'tempo': tempo, 'time_signature': timesig,
+	#		'key': key, 'mode': mode, 'duration': duration}
+	#return {'hash': int(hash(tid) & 0xfffffff), 'genre': genre, 'tid': tid, 'features': np.array([loudness, tempo, timesig, key, mode, duration])}
+	return LabeledPoint(gid, Vectors.dense(features))
